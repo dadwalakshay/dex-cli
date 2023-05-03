aa420ed55e1f383d60ceed0e9fff02a1c2736a38
dadwalakshay <akshayd00@outlook.com>
2023-05-04 00:19:52 +0530

    _meta.json added to config and reused

--- dex/db.py
+++ dex/db.py
@@ -1,8 +1,10 @@
 import json
 
+from dex.config import _META_STORE
+
 
 def create_chapter_meta(_meta_path: str, manga_title: str, chapter_title: str):
-    with open(f"{_meta_path}/_meta.json", "w+") as _meta_json:
+    with open(f"{_meta_path}/{_META_STORE}", "w") as _meta_json:
         _meta_json.write(
             json.dumps(
                 {"manga": manga_title, "chapter": chapter_title, "last_read_at": ""}
--- dex/prompts.py
+++ dex/prompts.py
@@ -7,7 +7,7 @@ from rich.console import Console
 from rich.prompt import Confirm, Prompt
 
 from dex.client import MangaDexClient
-from dex.config import DEFAULT_STORAGE_PATH
+from dex.config import _META_STORE, DEFAULT_STORAGE_PATH
 from dex.utils import _get_dirs, _open_chapter
 
 console = Console()
@@ -96,7 +96,7 @@ def confirm_download_prompt(
 def confirm_read_prompt(chapter_path: str) -> bool:
     is_read = False
 
-    _meta_path = f"{chapter_path}/_meta.json"
+    _meta_path = f"{chapter_path}/{_META_STORE}"
 
     with open(_meta_path, "r") as _meta_json_r:
         _meta_json_obj = json.loads(_meta_json_r.read())
@@ -124,7 +124,7 @@ def ls_dir(path: str = "") -> None:
 
     paths = os.listdir(curr_path)
 
-    if "_meta.json" in paths:
+    if _META_STORE in paths:
         if confirm_read_prompt(curr_path):
             raise typer.Exit(code=0)
 
