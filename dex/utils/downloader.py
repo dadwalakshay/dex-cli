f5a6db806d5039376d083398b4331b03206a389a
dadwalakshay <akshayd00@outlook.com>
2023-05-07 01:11:44 +0530

    multi-threading support added to BulkDownloader class

--- dex/utils/downloader.py
+++ dex/utils/downloader.py
@@ -1,57 +1,65 @@
 import os
+from multiprocessing.pool import ThreadPool
 
 import requests
-from rich.progress import track
 
 
 class BulkDownloader:
-    def __init__(
-        self, links: list[str], path: str, pool_size: int = 5, batch_size: int = 10
-    ) -> None:
+    def __init__(self, links: list[str], path: str) -> None:
         self.links = links
         self.path = path
 
-        self.pool_size = pool_size
-        self.batch_size = batch_size
-
-    def download(self, mode: str = "seq") -> list[str]:
+    def download(self, mode: str = "th") -> list[str]:
         match mode:
             case "seq":
-                _filenames = self.seq_download(self.links, self.path)
+                _filenames = self.seq_download()
+            case "th":
+                _filenames = self.th_download()
             case _:
                 raise NotImplementedError("Un-supported download mode.")
 
         return _filenames
 
-    @classmethod
-    def seq_download(cls, links: list[str], path: str) -> list[str]:
+    @staticmethod
+    def _save_link(page_args: tuple[int, str, str]) -> str:
+        count = page_args[0]
+        link = page_args[1]
+        path = page_args[2]
+
+        raw_filename = link.split("/")[-1]
+
+        ordered_filename = f"{str(count).zfill(3)}-{raw_filename}"
+
+        file_path = f"{path}/{ordered_filename}"
+
+        if not os.path.exists(file_path):
+            with requests.get(link, stream=True) as r_ctx:
+                r_ctx.raise_for_status()
+
+                with open(file_path, "wb") as page:
+                    for chunk in r_ctx.iter_content(chunk_size=8192):
+                        page.write(chunk)
+
+        return ordered_filename
+
+    def seq_download(self) -> list[str]:
         _filenames = []
 
-        for page_count, page_link in track(
-            enumerate(links, start=1), description="Downloading chapter..."
-        ):
-            raw_filename = page_link.split("/")[-1]
-
-            ordered_filename = f"{str(page_count).zfill(3)}-{raw_filename}"
-
-            file_path = f"{path}/{ordered_filename}"
-
-            if not os.path.exists(file_path):
-                with requests.get(page_link, stream=True) as r_ctx:
-                    r_ctx.raise_for_status()
-
-                    with open(file_path, "wb") as page:
-                        for chunk in r_ctx.iter_content(chunk_size=8192):
-                            page.write(chunk)
-
-            _filenames.append(ordered_filename)
+        for page_count, page_link in enumerate(self.links, start=1):
+            _filenames.append(self._save_link((page_count, page_link, self.path)))
 
         return _filenames
 
-    @classmethod
-    def th_download(cls, pool_size: int, batch_size: int) -> list[str]:
-        return []
+    def th_download(self) -> list[str]:
+        _fieldnames = []
 
-    @classmethod
-    def mp_download(cls, pool_size: int, batch_size: int) -> list[str]:
-        return []
+        pages = (
+            (page_count, page_link, self.path)
+            for page_count, page_link in enumerate(self.links, start=1)
+        )
+
+        with ThreadPool() as th_pool:
+            for result in th_pool.map(self._save_link, pages):
+                _fieldnames.append(result)
+
+        return _fieldnames
