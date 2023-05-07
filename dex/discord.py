c45930db8d0311b0960df8c89563d556a7f2413c
dadwalakshay <akshayd00@outlook.com>
2023-05-08 01:06:18 +0530

    DEFAULT_PDF_NAME added to discord file path

--- dex/discord.py
+++ dex/discord.py
@@ -1,6 +1,6 @@
 import requests
 
-from dex.config import DEFAULT_DISCORD_WEBHOOK_URL
+from dex.config import DEFAULT_DISCORD_WEBHOOK_URL, DEFAULT_PDF_NAME
 
 
 class DiscordClient:
@@ -8,7 +8,7 @@ class DiscordClient:
         self.path = path
 
     def send_file_webhook(self) -> None:
-        with open(f"{self.path}/dl.pdf", "rb") as _file:
+        with open(f"{self.path}/{DEFAULT_PDF_NAME}", "rb") as _file:
             requests.post(
                 DEFAULT_DISCORD_WEBHOOK_URL,
                 files={"files": _file},
