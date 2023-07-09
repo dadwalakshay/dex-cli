9af4b50de367b5263312fa8ba342bb0ee7c75e67
dadwalakshay <akshayd00@outlook.com>
2023-07-09 19:35:38 +0530

    last_read_at meta-data show against manga folders

--- dex/db.py
+++ dex/db.py
@@ -3,6 +3,18 @@ import json
 from dex.config import _META_STORE
 
 
+def read_chapter_meta(chapter_path: str) -> dict:
+    _meta_path = f"{chapter_path}/{_META_STORE}"
+
+    try:
+        with open(_meta_path, "r") as _meta_json_r:
+            _meta_json_obj = json.loads(_meta_json_r.read())
+
+            return _meta_json_obj
+    except FileNotFoundError:
+        return {}
+
+
 def create_or_update_chapter_meta(
     _meta_path: str, manga_title: str, chapter_title: str, _meta: dict
 ) -> None:
--- dex/prompts.py
+++ dex/prompts.py
@@ -13,6 +13,7 @@ from dex.config import (
     DEFAULT_STORAGE_PATH,
     IS_DISCORD_ENABLED,
 )
+from dex.db import read_chapter_meta
 from dex.discord import DiscordClient
 from dex.integrations.base import BaseClient
 from dex.utils import _get_dirs, _open_file
@@ -93,11 +94,13 @@ def confirm_read_prompt(chapter_path: str) -> bool:
     with open(_meta_path, "r") as _meta_json_r:
         _meta_json_obj = json.loads(_meta_json_r.read())
 
+        last_read_at = _meta_json_obj.get("last_read_at", "")
+
         if Confirm.ask(
             "Do you want to read"
             f" {_meta_json_obj['manga_title']} -"
-            f" {_meta_json_obj['chapter_title']}? - Last read at"
-            f" {_meta_json_obj['last_read_at']}"
+            f" {_meta_json_obj['chapter_title']}?"
+            f" {('- Last read at ' + last_read_at) if last_read_at else ''}"
         ):
             _open_file(f"{chapter_path}/{DEFAULT_PDF_NAME}")
 
@@ -118,8 +121,9 @@ def ls_dir(path: str = "") -> None:
     paths = os.listdir(curr_path)
 
     if _META_STORE in paths:
-        if confirm_read_prompt(curr_path):
-            raise typer.Exit(code=0)
+        confirm_read_prompt(curr_path)
+
+        ls_dir(f"{curr_path}/../")
 
     _dirs = sorted(_get_dirs(curr_path, paths))
 
@@ -132,7 +136,14 @@ def ls_dir(path: str = "") -> None:
     for choice, _dir in enumerate(_dirs, 1):
         choice_map[str(choice)] = _dir
 
-        console.print(f"({choice}) {_dir}")
+        chapter_meta = read_chapter_meta(f"{curr_path}/{_dir}")
+
+        last_read_at = chapter_meta.get("last_read_at", "")
+
+        console.print(
+            f"({choice})"
+            f" {_dir} {('- Last read at ' + last_read_at) if last_read_at else ''}"
+        )
 
     _selected_dir = choice_map[
         Prompt.ask(
